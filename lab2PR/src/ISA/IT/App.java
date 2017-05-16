955a182ebb4a5eebb0c76e4a49a055b6e5c59bfc
Vîrlan Ion Petru <Vrlan Ion Petru>
2017-05-16 15:20:59 +0400

    corectarea laboratorului 2

--- lab2PR/src/ISA/IT/App.java
+++ lab2PR/src/ISA/IT/App.java
@@ -6,36 +6,45 @@ import java.util.concurrent.CountDownLatch;
 public class App {
     public static void main(String[] args) {
 
-        CountDownLatch dependency = new CountDownLatch(2);
+        CountDownLatch dependency1_2 = new CountDownLatch(1);
 
-//3
+        CountDownLatch dependency2_5 = new CountDownLatch(1);
 
-        new Activity(Optional.of(() -> System.out.println("Action in 3")),Optional.of(dependency),Optional.empty()).start();
+        CountDownLatch dependency5_3 = new CountDownLatch(1);
 
-//6
+        CountDownLatch dependency6_4 = new CountDownLatch(1);
 
-        new Activity(Optional.of(() -> System.out.println("Action in 6")),Optional.of(dependency),Optional.empty()).start();
+        CountDownLatch dependency4_7 = new CountDownLatch(1);
 
-//4
-
-        new Activity(Optional.of(() -> System.out.println("Action in 4")),Optional.of(dependency),Optional.empty()).start();
-
-//7
-
-        new Activity(Optional.of(() -> System.out.println("Action in 7")),Optional.of(dependency),Optional.empty()).start();
+        CountDownLatch dependency7_3 = new CountDownLatch(1);
 
 //1
 
-        new Activity(Optional.of(() -> System.out.println("Action in 1")),Optional.empty(),Optional.of(dependency)).start();
+        new Activity(Optional.of(() -> System.out.println("Action in 1")),Optional.empty(),Optional.of(dependency1_2)).start();
 
 //2
 
-        new Activity(Optional.of(() -> System.out.println("Action in 2")),Optional.empty(),Optional.of(dependency)).start();
+        new Activity(Optional.of(() -> System.out.println("Action in 2")),Optional.of(dependency1_2),Optional.of(dependency2_5)).start();
 
 //5
 
-        new Activity(Optional.of(() -> System.out.println("Action in 5")),Optional.empty(),Optional.of(dependency)).start();
+        new Activity(Optional.of(() -> System.out.println("Action in 5")),Optional.of(dependency2_5),Optional.of(dependency5_3)).start();
 
+//6
+
+        new Activity(Optional.of(() -> System.out.println("Action in 6")),Optional.of(dependency5_3),Optional.of(dependency6_4)).start();
+
+//4
+
+        new Activity(Optional.of(() -> System.out.println("Action in 4")),Optional.of(dependency6_4),Optional.of(dependency4_7)).start();
+
+//7
+
+        new Activity(Optional.of(() -> System.out.println("Action in 7")),Optional.of(dependency4_7),Optional.of(dependency7_3)).start();
+
+//3
+
+        new Activity(Optional.of(() -> System.out.println("Action in 3")),Optional.of(dependency7_3),Optional.empty()).start();
 
     }
 }
--- lab2PR/src/ISA/IT/Main.java
+++
@@ -1,9 +0,0 @@
-package ISA.IT;
-
-
-public class Main {
-    public static void main(String[] args) {
-
-
-    }
-}
